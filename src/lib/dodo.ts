83ae10208f8c76373a457373d9629f7d3f6f5922
sirbxueonline-arch <sirbxueonline@gmail.com>
2026-01-20 13:24:26 +0400

    d

--- src/lib/dodo.ts
+++ src/lib/dodo.ts
@@ -1,6 +1,6 @@
 import DodoPayments from 'dodopayments';
 
 export const dodo = new DodoPayments({
-    bearerToken: process.env.DODO_PAYMENTS_API_KEY, // This is your API key
+    bearerToken: process.env.DODO_PAYMENTS_API_KEY || 'dummy_key', // This is your API key
     environment: process.env.NODE_ENV === 'production' ? 'live_mode' : 'test_mode', // Defaults to 'live'
 });
